bb3a9ef3a97d3df56d6dcd7b5993a332b58f493b
Lanmord <kostya.myndra@yandex.by>
2021-11-03 03:18:42 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [selectedMode, setSelectedMode] = useState(null);
+  const [modes, setModes] = useState(null);
+  const [fieldSize, setFieldSize] = useState(null);
+  const [hovers, setHovers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://demo1030918.mockable.io/').then(({ data }) => {
+      setModes(data);
+    });
+  }, []);
+
+  const onSquareHover = (row, col, element) => {
+    if (element.classList.contains('active')) element.classList.remove('active');
+    else element.classList.add('active');
+    setHovers([...hovers, `row:${row} col:${col}`]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="flex menu">
+        <select name="cars" onChange={(e) => setSelectedMode(e.target.value)}>
+          <option label="PICK MODE" />
+          {modes &&
+            Object.keys(modes).map((item) => (
+              <option key={item} value={item}>
+                {item.toUpperCase()}
+              </option>
+            ))}
+        </select>
+        <button
+          className="btn"
+          onClick={() => (selectedMode ? setFieldSize(modes[selectedMode].field) : null)}>
+          START
+        </button>
+      </div>
+      {fieldSize && (
+        <div className="flex">
+          <div>
+            {Array(fieldSize)
+              .fill(null)
+              .map((_, row) => (
+                <div key={row} className="flex">
+                  {Array(fieldSize)
+                    .fill(null)
+                    .map((_, col) => (
+                      <div
+                        key={col}
+                        className="square"
+                        onMouseOver={(e) => onSquareHover(row + 1, col + 1, e.target)}></div>
+                    ))}
+                </div>
+              ))}
+          </div>
+          {hovers.length !== 0 && (
+            <div className="hover">
+              <h2>Hover squares</h2>
+              {hovers
+                .slice(0)
+                .reverse()
+                .map((item, idx) => (
+                  <div key={idx} className="hover_item">
+                    {item}
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
